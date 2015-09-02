878d70c2f2dc273c5bd9e3d878eacf294bab252b
Andrew Savelev <savelevcorr@gmail.com>
2015-09-02 15:33:00 +1000

    load files async

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
-$(document).ready(function () {
-    $(document).foundation();
+head.load([
+    "/js/vendor/jquery.min.js",
+    "/js/vendor/foundation/foundation.min.js",
+    "/js/vendor/mask/jquery.inputmask.js",
+    "/js/vendor/owlcarousel/owl.carousel.min.js",
+    "/js/vendor/blueimp/jquery.blueimp-gallery.min.js",
+    "/js/vendor/handlebars.js",
+    "/js/vendor/jstorage.js"
+], function () {
+    // start
+    $(document).ready(function () {
+        $(document).foundation();
+        console.log('jquery', $);
+    });
+
 });
