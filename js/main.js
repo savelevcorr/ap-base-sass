946a2598f7d48d3e0f30347701593081d93d654c
Andrew Savelev <savelevcorr@gmail.com>
2015-09-02 17:07:19 +1000

    remove head.js & added modernizr

--- js/main.js
+++ js/main.js
@@ -1,16 +1,18 @@
-head.load([
-    "/js/vendor/jquery.min.js",
-    "/js/vendor/foundation/foundation.min.js",
-    "/js/vendor/mask/jquery.inputmask.js",
-    "/js/vendor/owlcarousel/owl.carousel.min.js",
-    "/js/vendor/blueimp/jquery.blueimp-gallery.min.js",
-    "/js/vendor/handlebars.js",
-    "/js/vendor/jstorage.js"
-], function () {
-    // start
-    $(document).ready(function () {
-        $(document).foundation();
-        console.log('jquery', $);
-    });
-
+Modernizr.load({
+    load: [
+        "/js/vendor/jquery.min.js",
+        "/js/vendor/foundation/foundation.min.js",
+        "/js/vendor/mask/jquery.inputmask.js",
+        "/js/vendor/owlcarousel/owl.carousel.min.js",
+        "/js/vendor/blueimp/jquery.blueimp-gallery.min.js",
+        "/js/vendor/handlebars.js",
+        "/js/vendor/jstorage.js"
+    ],
+    complete: function () {
+        // start
+        $(document).ready(function () {
+            $(document).foundation();
+            console.log('jquery', $);
+        });
+    }
 });
