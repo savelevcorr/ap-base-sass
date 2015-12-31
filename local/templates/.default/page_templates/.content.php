29e898286d469e0e9c8a0073d20ed2cad0aad5c9
Andrey Savelev <savelevcorr@gmail.com>
2015-12-31 16:37:37 +1000

    Add bower comoponents to .gitignore

--- local/templates/.default/page_templates/.content.php
+++
@@ -1,5 +0,0 @@
-<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?><?
-IncludeTemplateLangFile(__FILE__);
-$TEMPLATE["standard.php"] = array('name'=>"1 колонка", "sort"=>1);
-$TEMPLATE["standard2.php"] = array('name'=>"2 колонки", "sort"=>2);
-?>
